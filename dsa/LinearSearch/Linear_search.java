a14956bcd28271cd96c4523d86b3a184599b27e7
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-10 16:28:46 +0530

    Create Linear_search.java

---
+++ dsa/LinearSearch/Linear_search.java
@@ -0,0 +1,17 @@
+package com.dsa.LinearSearch;
+
+public class Linear_search {
+    static int linear_s(int[] a, int target) {
+        if (a.length == 0) return -1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == target) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6, 99, 5, 70, 2, 88, 33, 9};
+        int target = 70;
+        System.out.println(linear_s(nums, target));
+    }
+}
