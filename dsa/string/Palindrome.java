7c73cf16be5661e9b4a89d52db73df6378f2ac7b
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-21 17:05:36 +0530

    Update Palindrome.java

--- dsa/string/Palindrome.java
+++ dsa/string/Palindrome.java
@@ -1,12 +1,11 @@
 class Solution {
   public boolean isPalindrome(String s){
   s=s.toLowerCase();
-  if(s==null || s.length==0)
-    return true
+  
   for(int i=0;i<s.length()/2;i++) 
       { char start=s.charAt(i);
        char end=s.charAt(s.length-1-i);
-       if(start!=e) return false;
+       if(start!=end) return false;
       }
     return true;
   }
