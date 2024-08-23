a3fc66037a3927f12571f62a69e301473f7721a6
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-23 17:35:33 +0530

    Create MinTimeDifference.java

---
+++ dsa/string/MinTimeDifference.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        int n=timePoints.size();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+           String ctime=timePoints.get(i);
+
+            int val=Integer.parseInt(ctime.substring(0,2))*60;
+              int time=Integer.parseInt(ctime.substring(3));
+              arr[i]=val+time;
+        }
+        Arrays.sort(arr);
+
+        int min=(int)1e9;
+
+        for(int i=1;i<n;i++)
+        {
+            min=Math.min(min, arr[i]-arr[i-1]);
+        }
+         min=Math.min(min, (24*60 - arr[n-1])+arr[0]);
+          return min;
+    }
+   
+
+}
