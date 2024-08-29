37eb82174c0bf07aaeca676888eefe74e56077d8
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-29 22:58:35 +0530

    Update Factors.java

--- dsa/Factors.java
+++ dsa/Factors.java
@@ -2,19 +2,42 @@ package com.dsa;
 
 import java.util.Scanner;
 
-public class Factors {
-    static void factors(int n){
-        System.out.println("Factors of "+n+" are : ");
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                System.out.print(i+ ", ");
-            }
+public class HCF_LCM {
+    //gcd
+    static int hcf(int n,int m){
+        int h=0;
+        for(int i = 1; i <= n || i <= m; i++) {
+            if( n%i == 0 && m%i == 0 )
+                h = i;
         }
+        return h;
+    }
+    static int gcd(int a,int b){
+
+        if(a==0)return b;
+
+        return gcd(b%a,a);//euclid algo
+    }
+    //lcm
+    static int lcm(int a,int b) {
+        /*int l = Math.max(n, m);
+        while (true) {
+            if (l % n == 0 && l % m == 0)
+                return l;
+            ++l;
+            */
+
+           return a*b/gcd(a,b);
+
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number : ");
-        int n = sc.nextInt();
-        factors(n);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two number : ");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        //System.out.println("HCF of "+n+" is "+hcf(n,m));
+        System.out.println("LCM of "+n+" is "+lcm(n,m));
+        System.out.println("gcd "+gcd(3,9));
     }
 }
+
