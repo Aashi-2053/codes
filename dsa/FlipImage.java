a6e3be011a144a89156c2913f7e25b0231818ad2
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-29 17:09:10 +0530

    Create FlipImage.java

---
+++ dsa/FlipImage.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/flipping-an-image/
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] r:image)
+        {
+            for(int i=0;i<(image[0].length+1)/2;i++)
+            {
+                int temp=r[i]^1;
+                r[i]=r[image[0].length-i-1]^1;
+                r[image[0].length-i-1]=temp;
+
+            }
+        }
+        return image;
+    }
+}
