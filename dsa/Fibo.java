35c00e0a4a1450d83f40ce45df32609b86679c51
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-08-30 17:47:54 +0530

    Update Fibo.java

--- dsa/Fibo.java
+++ dsa/Fibo.java
@@ -1,6 +1,5 @@
 package com.dsa;
 
-import javax.swing.plaf.IconUIResource;
 import java.util.Scanner;
 
 public class Fibo {
@@ -9,6 +8,10 @@ public class Fibo {
         System.out.print("Enter number : ");
         int n = sc.nextInt();
         fibo_Series(n);
+        //using formula
+        System.out.println();
+        for(int i=1;i<n;i++)
+        System.out.print(fiboFormula(i)+" ");
     }
     static void fibo_Series(int n){
         int a=0,b=1;
@@ -20,5 +23,8 @@ public class Fibo {
             b=sum;
             sum=a+b;
         }
+    }
+    static int fiboFormula(int n){
+        return (int) (Math.pow(((1+Math.sqrt(5))/2),n)/Math.sqrt(5));
     }
 }
