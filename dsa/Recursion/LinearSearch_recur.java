aaa67e0f91a4e532f2222a54e5dee44dc265dd45
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-09-02 16:47:24 +0530

    Update LinearSearch_recur.java

--- dsa/Recursion/LinearSearch_recur.java
+++ dsa/Recursion/LinearSearch_recur.java
@@ -1,5 +1,7 @@
 package com.dsa.recur;
 
+import java.util.ArrayList;
+
 public class LinearSearch_recur {
     static int findIndex(int[] a,int k,int i){
         if(i==a.length){
@@ -7,6 +9,23 @@ public class LinearSearch_recur {
         }
        if(a[i]==k) return i;
        return findIndex(a,k,i+1);
+
+    }
+    static int findLastIndex(int[] a,int k,int i){
+        if(i==-1){
+            return -1;
+        }
+        if(a[i]==k) return i;
+        return findIndex(a,k,i--);
+
+    }
+    static ArrayList<Integer> list=new ArrayList<>();
+    static void findAllIndex(int[] a,int k,int i){
+        if(i==a.length){
+            return ;
+        }
+        if(a[i]==k)list.add(i);
+        findAllIndex(a,k,i+1);
 
     }
     static boolean find(int[] a,int k,int i){
@@ -17,8 +36,11 @@ public class LinearSearch_recur {
 
     }
     public static void main(String[] args) {
-        int [] a={3,2,1,18,9};
+        int [] a={3,2,1,18,1,9};
         System.out.println(findIndex(a,18,0));
         System.out.println(find(a,18,0));
+        System.out.println(findLastIndex(a,1,a.length-1));
+        findAllIndex(a,1,0);
+        System.out.println(list);
     }
 }
