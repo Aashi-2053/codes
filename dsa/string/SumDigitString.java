a74ae3ce648cb5e73f716042ff78973c2b4a6e58
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-09-03 16:27:55 +0530

    Create SumDigitString.java

---
+++ dsa/string/SumDigitString.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/sum-of-digits-of-string-after-convert/description/
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+       int sum=0;
+        for(int i=0;i<s.length();i++)
+        {       
+            sb.append(((int)(s.charAt(i)) - 96));
+        }
+        for(int i=0;i<sb.length();i++)
+        {
+             sum += ((sb.charAt(i))-'0');
+        }
+       k--;
+        while(k!=0){
+            int num = sum;
+            sum = 0;
+            while(num!=0){
+                int a = num%10;
+                num/=10;
+                sum += a;
+            }
+            k--;
+        }
+        return sum;
+
+    }
+}
