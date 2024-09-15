e9232b4335a75d9fd5beff6aec5144c4d3923a8b
Aashi Tiwari <129379312+Aashi-2053@users.noreply.github.com>
2024-09-15 23:53:19 +0530

    Update HasCycle.java

--- dsa/LinkedList/HasCycle.java
+++ dsa/LinkedList/HasCycle.java
@@ -26,3 +26,25 @@ public class Solution {
         return false;
     }
 }
+//length of Linked List
+   public int CycleLength(Node head) {
+            Node fast=head;
+            Node slow=head;
+            if(head==null)
+                return false;
+            while(fast!=null && fast.next!=null)
+            {
+                fast=fast.next.next;
+                slow=slow.next;
+                if(slow==fast) {
+                    ListNode temp= slow;
+                    int len=0;
+                    do{
+                        temp=temp.next;
+                        len++;
+                    }while (temp!=slow);
+                    return len;
+                }
+            }
+            return 0;
+        }
